946a60039ea5e78295fed0d1895c745c09e64344
akmatkulov <akmatkulovs991@gmail.com>
2023-09-24 16:46:46 +0600

    Initial App 3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+   # Return full title
+   def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    return page_title + ' | ' + base_title if page_title.present?
+    return base_title
+  end
 end
